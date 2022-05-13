ac8041bca8f9f51f87a926032df770e7a3194695
kheesu <ingan7157@gmail.com>
2022-05-13 15:07:46 +0900

    Prototype to header

--- Project1/header.h
+++ Project1/header.h
@@ -16,3 +16,30 @@ typedef struct head {
     int config[2];                          //config[0] == number of columns, config[1] == minimum score
 } head;
 
+head* table_init(int len);
+
+void row_append(int q, head* pointer);
+
+void print_row(row* r, int q);
+
+void row_del(head* table, int q, int column, int query);
+
+
+void csv_out(head* table, int q, char* name);
+
+head* csv_in(const char* name);
+
+
+void get_average(head* table, int q);
+
+row* return_max_average_row(const head* table, int q, int floor_score, int* pass_people_array, int pass_people_array_cnt);
+
+int check_floor_score(row* using_row, int q, int floor_score);
+
+void score_people(head* table, int q, int floor_score, int pass_people_num);
+
+int is_in_array(int* Array, int Array_length, int source);
+
+int how_many_people(head* table);
+
+void iterator(head* headrow, int q);
